e4b0ea2e94f94574221a5e8c47c7835d15534d38
Siddhant Mohile <60550481+SidmoGoesBrrr@users.noreply.github.com>
2022-06-29 17:39:09 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='zocrypt',
-    version='1.1.7',
+    version='1.1.8',
     license='MIT',
     author="Siddhant Mohile",
     author_email='meetsiddhant@gmail.com',
-    packages=find_packages('zocrypt'),
+    packages=find_packages(where="src"),
     package_dir={'': 'src'},
     url='https://github.com/SidmoGoesBrrr/zocrypt',
     keywords=['encrypt','decrypt','encoding','secret messages'],
